836c5327e0cd400ad0323a885c64ee0634da8a09
Albo Vieira <albovieira@gmail.com>
2017-01-24 21:45:51 -0200

    starting

--- PhpCertificate/FileSystemIO/FileGetContents.php
+++ PhpCertificate/FileSystemIO/FileGetContents.php
@@ -9,13 +9,6 @@
 class FileGetContents
 {
 
-    public function createContext($options, $params){
-        return stream_context_create($options,$params);
-    }
 
-    public function read($path){
-        $content = file_get_contents($path);
-        return $content;
-    }
 
 }
